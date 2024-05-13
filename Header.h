53ad86a9dc1872ada6a50cd5ee1f7fd13b194cc3
DLSUliogarcia <aurelio_garcia@dlsu.edu.ph>
2024-05-13 15:59:26 +0800

    Basic Setup Explains what each function is supposed to do

---
+++ Header.h
@@ -0,0 +1,16 @@
+#ifndef CSOPESY
+#define CSOPESY
+
+// marquee function: Prints string ""marquee" command recognized. Doing something.”
+
+// screen function: Prints string ""screen" command recognized. Doing something.”
+
+// process-smi function: Prints string ""process-smi" command recognized. Doing something.”
+
+// nvidia-smi function: Prints string ""nvidia-smi" command recognized. Doing something.”
+
+// clear function: Clears screen, reprinting the header texts.
+
+// exit function: Closes application/CLI
+
+#endif CSOPESY
