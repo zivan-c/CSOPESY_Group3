14fe6448d3442053b148fa85f6a5aa8c41003e45
Mich <91170055+michhandreaa@users.noreply.github.com>
2024-05-30 15:50:46 +0800

    fixing vs sln problem

--- Header.h
+++ Header.h
@@ -1,16 +1,3 @@
-#ifndef CSOPESY
-#define CSOPESY
+#include "Console.cpp"
 
-// marquee function: Prints string ""marquee" command recognized. Doing something.”
 
-// screen function: Prints string ""screen" command recognized. Doing something.”
-
-// process-smi function: Prints string ""process-smi" command recognized. Doing something.”
-
-// nvidia-smi function: Prints string ""nvidia-smi" command recognized. Doing something.”
-
-// clear function: Clears screen, reprinting the header texts.
-
-// exit function: Closes application/CLI
-
-#endif CSOPESY
