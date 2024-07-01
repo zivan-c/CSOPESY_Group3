f2722c68bf0322b5557dd95b823e527ee1854d12
Mich <91170055+michhandreaa@users.noreply.github.com>
2024-07-01 15:55:43 +0800

    fix 2

---
+++ marquee.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <conio.h>
+#include "marqueeWorkerThread.h"
+#include "functions.h"
+#include "Windows.h"
+
+void marquee() {
+
+}
---
+++ scheduler.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+void schedulerStop() {
+
+}
+
+void schedulerTest() {
+
+}
