a773bf6a7479d385c38763b5a3c0e79e8a6ff0e6
Mich <91170055+michhandreaa@users.noreply.github.com>
2024-07-01 15:59:13 +0800

    delete files - clean slate

--- marquee.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "functions.h"
-
-// marquee function: Prints string ""marquee" command recognized. Doing something.”
-void marquee() {
-    std::cout << "\nmarquee command recognized. Doing something.\n";
-}
--- nvidia-smi.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "functions.h"
-
-// nvidia-smi function: Prints string ""nvidia-smi" command recognized. Doing something.”
-void nvidiaSmi() {
-	std::cout << "\nnvidia-smi command recognized. Doing something.\n";
-}
